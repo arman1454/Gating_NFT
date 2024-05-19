7503930217ddbe44ed30a285c28cdb7885d99ac1
arman <fakidarman@gmail.com>
2024-05-19 09:53:48 -0400

    web3 with quickNode api, contract address, contract instance and NftFetching function

--- server/server.js
+++ server/server.js
@@ -7,10 +7,24 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+const web3 =new Web3('YOUR API KEY')
+
+const contractAddress = 'YOUR CONTRACT ADDRESS';
+
+const contract = new web3.eth.Contract(ABI,contractAddress);
+
+const fetchNFTs = async(account)=>{
+    try{
+       const nftBalance = await contract.methods.balanceOf(account).call();
+       return {userNFTs:Number(nftBalance)}
+    }catch(error){
+       console.log('Error fetching NFTs',error);
+    }
+}
+
+
 
 const PORT=3000;
 const server = app.listen(PORT,()=>{
     console.log(`Sever running at ${PORT}`)
 })
-
-//This is a comment dasdasdasdasda
