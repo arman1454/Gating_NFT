4eef5e775aa2e17ef381f79f26d3f6db21b66d8f
arman <fakidarman@gmail.com>
2024-05-19 09:13:55 -0400

    server initialized

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors')
+const {Web3} = require('web3');
+const ABI =require('./ABI.json')
+const socketIO = require('socket.io')
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+
+const PORT=3000;
+const server = app.listen(PORT,()=>{
+    console.log(`Sever running at ${PORT}`)
+})
